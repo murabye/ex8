2ccd92c6475cab420c25d89fa1c4ec2c7c759590
murabye <vlada_kuz2011@mail.ru>
2017-07-01 21:03:11 +0500

    из букв в цифры для удобства

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using MyLib;
 
 namespace ex8
@@ -6,23 +7,56 @@ namespace ex8
     internal class Program
     {
         private static int[][] list = new int[0][];     // граф
-
-        /*
-         *   procedure FindEulerPath (V)
-	     *   1. перебрать все рёбра, выходящие из вершины V;
-		 *           каждое такое ребро удаляем из графа, и
-		 *           вызываем FindEulerPath из второго конца этого ребра;
-	     *   2. добавляем вершину V в ответ.
-         *   
-         */
-
+        private static List<int> ans;
+        
         private static void FindEulerPath(int num)
         {
             // где num - номер соответствующей вершины графа
 
+            for (var i = 0; i < list[num].Length; i++)
+                if (list[num][i] != -1)
+                {
+                    var path = list[num][i];
+
+                    // удаление ребра из графа
+                    list[num][i] = -1;
+                    for (var j = 0; j < list[path].Length; j++)
+                    {
+                        if (list[path][j] != num) continue;
+
+                        list[path][j] = -1;
+                        break;
+                    }
+
+                    FindEulerPath(path);
+                }
+
+                        
+            ans.Add(num + 1);
 
         }
+        private static Queue<int> Check()
+        {
+            var odd = new Queue<int>();          // нечетные вершины
 
+            for (var i = 0; i < list.Length; i++)
+                if (list[i].Length % 2 != 0)
+                    odd.Enqueue(i);
+            if (odd.Count != 2) throw new ArgumentException("В графе нет эйлеровой цепи");
+
+            foreach (var point in odd)
+            {
+                var arr = (int[])list[point].Clone();
+
+                list[point] = new int[arr.Length + 1];
+                for (var i = 0; i < arr.Length; i++)
+                    list[point][i] = arr[i];
+
+                list[point][arr.Length] = point;
+            }
+
+            return odd;
+        }
 
         private static void Main(string[] args)
         {
@@ -33,7 +67,7 @@ namespace ex8
             // чтение списка
             for (var i = 0; i < number; i++)
             {
-                var letter = (char)('A' + i);
+                var letter = i + 1;
 
                 Console.Write("Введите через пробел вершины, с которыми связана ({0}): ", letter);
                 var relation = Console.ReadLine().Trim().Split(' ');       // читает и делит
@@ -44,16 +78,21 @@ namespace ex8
                 {
                     if (point[0] == letter) throw new ArgumentException("Нет задачи графа с петлей");
                     if (point.Length > 1) throw new ArgumentException("Более одного символа вершина не именуется");
-                    if ((int)point[0] - 'A'> 15) throw new ArgumentException("Превышение заданного алфавита");
+                    if ((int)point[0] - 'A' >= number) throw new ArgumentException("Не обнаружено вершины");
 
-                    list[i][j++] = point[0] - 'A';
+                    list[i][j++] = int.Parse(point[0].ToString());
                 }
             }
 
 
+            var odd = Check().ToArray();
+            FindEulerPath(0);
+            var ans1 = odd[0] + " " + odd[1];
+            var ans2 = odd[1] + " " + odd[0];
 
+            // ans.Remove(ans.IndexOf(ans1) > -1 ? ans.IndexOf(ans1) : ans.IndexOf(ans2), ans1.Length);
 
-
+            Console.WriteLine("Ответ: " + ans);
             OC.Stay();
         }
     }
